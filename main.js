4d4856790a36eaae17898e4a05bb5f02fee8a02a
Michael  Abraha <m.b.abraha@msmary.edu>
2020-06-19 03:43:04 +0000

    kind of complete

--- main.js
+++ main.js
@@ -9,39 +9,56 @@ let person = [{
 }]
 
 
+function capital(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
 function listCustomers(personArray) {
     let customerList = document.querySelector('#customer-list');
     console.log(customerList)
 
-    for (let customer of customers) {
-        let newCustomer = document.createElement("li");
 
-        let customerText = document.createTextNode(`${customer.name.title} ${customer.name.first} ${customer.name.last} located in: ${customer.location.city}, ${customer.location.state}       Date of Birth: ${customer.dob}, Date Joined: ${customer.registered}`);
+    for (let customer of customers) {
+
+        let newCustomer = document.createElement("div");
+
+        let customerText = document.createTextNode(`${capital(customer.name.title)} ${capital(customer.name.first)} ${capital(customer.name.last)}`);
+
+        let customerDob = document.createTextNode(`Date of Birth: ${customer.dob}, Date Joined: ${customer.registered}`);
+
+
+
+        let customerAddress = document.createTextNode(`${capital(customer.location.street)} ${capital(customer.location.city)},${capital(customer.location.state)}`);
 
         let customerEmail = document.createTextNode(`${customer.email}`);
 
+
+        let name = document.createElement("p");
         let address = document.createElement("p");
         let email = document.createElement("p");
+        let dob = document.createElement("p");
+
         address.classList.add("address", "measure", "lh-copy");
         email.classList.add("email", "measure", "2h-copy");
 
-        address.appendChild(customerText);
+        newCustomer.appendChild(customerText);
         email.appendChild(customerEmail);
-
-        newCustomer.append(address, email)
+        address.appendChild(customerAddress);
+        dob.appendChild(customerDob);
+        newCustomer.append(address, email);
 
         let customerImage = document.createElement("img");
         customerImage.classList.add("br-100", "h4", "w4", "dib", "ba", "b--black-05", "pa2");
         customerImage.src = `${customer.picture.thumbnail}`;
 
-        // let customerEmail = document.createElement("email");
-        //newCustomer.append(address, customerEmail);
-
+       
+        newCustomer.append(name, email, address, dob);
         newCustomer.prepend(customerImage);
-        newCustomer.classList.add("ph3", "pv3", "bb", "b--light-silver");
+        //newCustomer.classList.add("ph3", "pv3", "bb", "b--light-silver");
         customerList.append(newCustomer);
+
+
     };
 
 
